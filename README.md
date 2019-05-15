225155043c16592e0e4f014ee119d073a82ee4ba
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-16 01:54:13 +0700

    Complete send mail using node-cron

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ $ yarn
 # Run app
 $ yarn start
 ```
+
+*Need to allow non secure apps to access gmail you can do this by going to your gmail settings [here](https://myaccount.google.com/lesssecureapps).*
--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+require('dotenv').config();
+
 const express = require('express');
 const cron = require('node-cron');
-const fs = require('fs');
+const nodemailer = require('nodemailer');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-cron.schedule('* * * * *', () => console.log('Running a task every minute'));
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ndc07.it@gmail.com',
+    pass: process.env.GMAIL_PASSWORD
+  }
+});
+
+cron.schedule('* * * * 7', () => {
+  let mailOptions = {
+    from: '"Cuong Duy Nguyen 👻" ndc07.it@gmail.com',
+    to: 'cuongw.me@gmail.com',
+    subject: 'Just test cron jobs',
+    text: 'Just test ............'
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('Send mail successfully');
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
